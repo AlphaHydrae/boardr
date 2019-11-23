704ea37abcfa1292fe8e3401483024c43e8c45a7
Simon Oulevay <simon.oulevay@gmail.com>
2019-11-23 19:02:53 +0100

    Link from api root to moves

--- server/lib/boardr_web/api/api_view.ex
+++ server/lib/boardr_web/api/api_view.ex
@@ -8,7 +8,8 @@ defmodule BoardrWeb.ApiView do
     |> put_hal_curies_link()
     |> put_hal_self_link(:api_url, [:index])
     |> put_hal_links(%{
-      'boardr:games' => Routes.games_url(BoardrWeb.Endpoint, :index)
+      'boardr:games' => Routes.games_url(BoardrWeb.Endpoint, :index),
+      'boardr:moves' => Routes.moves_url(BoardrWeb.Endpoint, :index),
     })
   end
 end
