375ff9cd8ea6d7c08e240053d698b4d9be923731
Simon Oulevay <simon.oulevay@gmail.com>
2019-12-13 21:44:15 +0100

    Performance testing example with locust

---
+++ performance/locustfile.py
@@ -0,0 +1,26 @@
+from locust import HttpLocust, TaskSet, between
+
+def login(l):
+    l.client.post("/login", {"username":"ellen_key", "password":"education"})
+
+def logout(l):
+    l.client.post("/logout", {"username":"ellen_key", "password":"education"})
+
+def index(l):
+    l.client.get("/")
+
+def profile(l):
+    l.client.get("/profile")
+
+class UserBehavior(TaskSet):
+    tasks = {index: 2, profile: 1}
+
+    def on_start(self):
+        login(self)
+
+    def on_stop(self):
+        logout(self)
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    wait_time = between(5.0, 9.0)
