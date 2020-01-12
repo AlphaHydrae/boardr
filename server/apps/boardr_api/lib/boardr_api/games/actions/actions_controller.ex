b7a6913b4d0fb5a8f90f22764c516477d3ca4907
Simon Oulevay <simon.oulevay@gmail.com>
2020-01-12 16:27:40 +0100

    Do not call game server to perform action when game is not playing

--- server/apps/boardr_api/lib/boardr_api/games/actions/actions_controller.ex
+++ server/apps/boardr_api/lib/boardr_api/games/actions/actions_controller.ex
@@ -1,26 +1,42 @@
 defmodule BoardrApi.Games.ActionsController do
   use BoardrApi, :controller
 
-  alias Boardr.{Action,Player,Repo}
+  alias Boardr.{Action, Player, Repo}
   alias Boardr.Gaming.GameServer
 
-  plug Authenticate, [:'api:games:update:actions:create'] when action in [:create]
-  plug Authenticate, [:'api:games:show:actions:index'] when action in [:index]
-  plug Authenticate, [:'api:games:show:actions:show'] when action in [:show]
+  plug(Authenticate, [:"api:games:update:actions:create"] when action in [:create])
+  plug(Authenticate, [:"api:games:show:actions:index"] when action in [:index])
+  plug(Authenticate, [:"api:games:show:actions:show"] when action in [:show])
 
-  def create(%Conn{assigns: %{auth: %{"sub" => user_id}}} = conn, %{"game_id" => game_id} = action_properties) do
-    player_id = Repo.one!(from(p in Player, select: p.id, where: p.game_id == ^game_id and p.user_id == ^user_id))
-    with {:ok, action} <- GameServer.play(game_id, player_id, Map.delete(action_properties, "game_id")) do
+  def create(
+        %Conn{assigns: %{auth: %{"sub" => user_id}}} = conn,
+        %{"game_id" => game_id} = action_properties
+      ) do
+    {player_id, game_state} =
+      Repo.one!(
+        from(p in Player,
+          join: g in assoc(p, :game),
+          select: {p.id, g.state},
+          where: p.game_id == ^game_id and p.user_id == ^user_id
+        )
+      )
+
+    with {:ok, action} <-
+           play(game_id, game_state, player_id, Map.delete(action_properties, "game_id")) do
       conn
       |> put_status(201)
       |> put_resp_content_type("application/hal+json")
-      |> put_resp_header("location", Routes.games_actions_url(Endpoint, :show, action.game_id, action.id))
+      |> put_resp_header(
+        "location",
+        Routes.games_actions_url(Endpoint, :show, action.game_id, action.id)
+      )
       |> render(%{action: action})
     end
   end
 
   def index(%Conn{} = conn, %{"game_id" => game_id}) do
-    actions = Repo.all from(a in Action, order_by: [desc: a.performed_at], where: a.game_id == ^game_id)
+    actions =
+      Repo.all(from(a in Action, order_by: [desc: a.performed_at], where: a.game_id == ^game_id))
 
     conn
     |> put_resp_content_type("application/hal+json")
@@ -28,10 +44,26 @@ defmodule BoardrApi.Games.ActionsController do
   end
 
   def show(conn, %{"game_id" => game_id, "id" => id}) do
-    action = Repo.one! from(a in Action, where: a.game_id == ^game_id and a.id == ^id)
+    action = Repo.one!(from(a in Action, where: a.game_id == ^game_id and a.id == ^id))
 
     conn
     |> put_resp_content_type("application/hal+json")
     |> render(%{action: action})
+  end
+
+  defp play(game_id, "waiting_for_players", player_id, action_properties)
+       when is_binary(game_id) and is_binary(player_id) and is_map(action_properties) do
+    {:error, {:game_error, :game_not_started}}
+  end
+
+  defp play(game_id, "playing", player_id, action_properties)
+       when is_binary(game_id) and is_binary(player_id) and is_map(action_properties) do
+    GameServer.play(game_id, player_id, Map.delete(action_properties, "game_id"))
+  end
+
+  defp play(game_id, game_state, player_id, action_properties)
+       when is_binary(game_id) and is_binary(game_state) and is_binary(player_id) and
+              is_map(action_properties) do
+    {:error, {:game_error, :game_finished}}
   end
 end
