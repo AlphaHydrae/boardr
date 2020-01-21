19e2732194f8777e87c8ad2452e258cf13874bfe
Simon Oulevay <simon.oulevay@gmail.com>
2020-01-21 21:16:24 +0100

    Implement swarm handoff to avoid extra database queries

--- server/apps/boardr/lib/boardr/gaming/game_server.ex
+++ server/apps/boardr/lib/boardr/gaming/game_server.ex
@@ -11,8 +11,21 @@ defmodule Boardr.Gaming.GameServer do
   require Logger
   require Record
 
-  @type game :: record(:state_record, game: Game.t | nil, rules: Rules.t, rules_game: Domain.game | nil, rules_state: map | nil)
-  Record.defrecord(:state_record, __MODULE__, game: nil, rules: nil, rules_game: nil, rules_state: nil)
+  @type game ::
+          record(:state_record,
+            game: Game.t() | nil,
+            game_id: binary,
+            rules: Rules.t(),
+            rules_game: Domain.game() | nil,
+            rules_state: map | nil
+          )
+  Record.defrecord(:state_record, __MODULE__,
+    game: nil,
+    game_id: nil,
+    rules: nil,
+    rules_game: nil,
+    rules_state: nil
+  )
 
   @default_timeout 5_000
 
@@ -42,7 +55,12 @@ defmodule Boardr.Gaming.GameServer do
   @impl true
   def init(game_id) when is_binary(game_id) do
     Process.flag(:trap_exit, true)
-    {:ok, nil, {:continue, {:init, game_id}}}
+    {:ok, state_record(game_id: game_id)}
+  end
+
+  @impl true
+  def handle_call(args, from, state_record(game: nil) = state) do
+    handle_call(args, from, load_state_from_database(state))
   end
 
   @impl true
@@ -97,7 +115,8 @@ defmodule Boardr.Gaming.GameServer do
       state_record(
         state,
         game: %Game{game | players: new_players, state: updated_game.state},
-        rules_game: Domain.game(rules_game, players: new_domain_players, state: updated_game.state)
+        rules_game:
+          Domain.game(rules_game, players: new_domain_players, state: updated_game.state)
       )
     )
   end
@@ -146,7 +165,10 @@ defmodule Boardr.Gaming.GameServer do
            ),
          {:ok, %{action: persisted_action, game: persisted_game}} <-
            persist_action(action, player, game, game_result) do
-      reply(persisted_action, state_record(state, game: persisted_game, rules_state: new_rules_state))
+      reply(
+        persisted_action,
+        state_record(state, game: persisted_game, rules_state: new_rules_state)
+      )
     else
       error ->
         error_reply(error, state)
@@ -167,7 +189,12 @@ defmodule Boardr.Gaming.GameServer do
   def handle_call(
         {:possible_actions, filters},
         _from,
-        state_record(game: %Game{state: "playing"} = game, rules: rules, rules_game: rules_game, rules_state: rules_state) = state
+        state_record(
+          game: %Game{state: "playing"} = game,
+          rules: rules,
+          rules_game: rules_game,
+          rules_state: rules_state
+        ) = state
       )
       when is_map(filters) do
     rules_filters = %{}
@@ -203,7 +230,8 @@ defmodule Boardr.Gaming.GameServer do
   end
 
   @impl true
-  def handle_call({:possible_actions, filters}, _from, state_record(game: game) = state) when is_map(filters) do
+  def handle_call({:possible_actions, filters}, _from, state_record(game: game) = state)
+      when is_map(filters) do
     reply({[], game}, state)
   end
 
@@ -211,10 +239,19 @@ defmodule Boardr.Gaming.GameServer do
   def handle_call(
         {:swarm, :begin_handoff},
         _from,
-        state_record(game: %Game{id: game_id}) = state
+        state_record(game_id: game_id) = state
       ) do
     Logger.debug("Swarm beginning handoff of game server for game #{game_id}")
     {:reply, :restart, state}
+  end
+
+  @impl true
+  def handle_cast(
+        {:swarm, :end_handoff, state_record() = handed_off_state},
+        state_record(game: nil, game_id: game_id)
+      ) do
+    Logger.debug("Swarm ending handoff of game server for game #{game_id}")
+    {:noreply, handed_off_state}
   end
 
   @impl true
@@ -226,71 +263,7 @@ defmodule Boardr.Gaming.GameServer do
   end
 
   @impl true
-  def handle_continue({:init, game_id}, _state) when is_binary(game_id) do
-    Logger.debug("Initializing game server for game #{game_id}")
-
-    players_query = from(p in Player, order_by: p.number)
-
-    game =
-      Repo.get!(Game, game_id)
-      |> Repo.preload(
-        players: players_query,
-        winners: []
-      )
-
-    rules = get_rules!(game.rules)
-
-    rules_players =
-      game.players
-      |> Enum.map(fn %Player{number: player_number} -> Domain.player(number: player_number) end)
-
-    rules_game =
-      Domain.game(
-        players: rules_players,
-        rules: game.rules,
-        settings: game.settings,
-        state: :playing
-      )
-
-    {:ok, {rules_state, number_of_actions}} =
-      Repo.transaction(fn ->
-        from(a in Action,
-          join: p in assoc(a, :player),
-          order_by: a.performed_at,
-          select: {a, p},
-          where: a.game_id == ^game_id
-        )
-        # FIXME: why does Repo.stream make 3 queries?
-        #|> Repo.stream(max_rows: 100)
-        |> Repo.all()
-        |> Enum.reduce({nil, 0}, fn {action, player}, {current_rules_state, n} ->
-          result =
-            rules.play(
-              Domain.take(
-                player_number: player.number,
-                position: Domain.position_from_list(action.position)
-              ),
-              rules_game,
-              current_rules_state
-            )
-
-          case result do
-            {:ok, _, new_state, _} -> {new_state, n + 1}
-          end
-        end)
-      end)
-
-    Logger.info("Initialized game server for game #{game_id} with #{number_of_actions} actions")
-
-    {
-      :noreply,
-      state_record(game: game, rules: rules, rules_game: rules_game, rules_state: rules_state),
-      @default_timeout
-    }
-  end
-
-  @impl true
-  def handle_info(:timeout, state_record(game: %Game{id: game_id}) = state) do
+  def handle_info(:timeout, state_record(game_id: game_id) = state) do
     Logger.info("Shutting down inactive game server for game #{game_id}")
     {:stop, {:shutdown, :timeout}, state}
   end
@@ -298,7 +271,7 @@ defmodule Boardr.Gaming.GameServer do
   @impl true
   def handle_info(
         {:swarm, :die},
-        state_record(game: %Game{id: game_id}) = state
+        state_record(game_id: game_id) = state
       ) do
     Logger.info("Swarm shutting down game server for game #{game_id}")
     {:noreply, state}
@@ -408,6 +381,65 @@ defmodule Boardr.Gaming.GameServer do
       |> Keyword.fetch!(:rules_factory)
 
     factory.get_rules(rules_name)
+  end
+
+  defp load_state_from_database(state_record(game_id: game_id) = state) do
+    Logger.debug("Initializing game server for game #{game_id}")
+
+    players_query = from(p in Player, order_by: p.number)
+
+    game =
+      Repo.get!(Game, game_id)
+      |> Repo.preload(
+        players: players_query,
+        winners: []
+      )
+
+    rules = get_rules!(game.rules)
+
+    rules_players =
+      game.players
+      |> Enum.map(fn %Player{number: player_number} -> Domain.player(number: player_number) end)
+
+    rules_game =
+      Domain.game(
+        players: rules_players,
+        rules: game.rules,
+        settings: game.settings,
+        state: :playing
+      )
+
+    {:ok, {rules_state, number_of_actions}} =
+      Repo.transaction(fn ->
+        from(a in Action,
+          join: p in assoc(a, :player),
+          order_by: a.performed_at,
+          select: {a, p},
+          where: a.game_id == ^game_id
+        )
+        # FIXME: why does Repo.stream make 3 queries?
+        # |> Repo.stream(max_rows: 100)
+        |> Repo.all()
+        |> Enum.reduce({nil, 0}, fn {action, player}, {current_rules_state, n} ->
+          result =
+            rules.play(
+              Domain.take(
+                player_number: player.number,
+                position: Domain.position_from_list(action.position)
+              ),
+              rules_game,
+              current_rules_state
+            )
+
+          case result do
+            {:ok, _, new_state, _} -> {new_state, n + 1}
+          end
+        end)
+      end)
+
+    Logger.info("Initialized game server for game #{game_id} with #{number_of_actions} actions")
+
+    state_record(state, game: game, rules: rules, rules_game: rules_game, rules_state: rules_state)
   end
 
   def error_reply(err, state_record() = state) do
