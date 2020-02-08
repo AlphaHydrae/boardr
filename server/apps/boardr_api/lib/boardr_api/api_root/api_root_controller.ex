89a0b14d3178cb8243c6bb392298c5c0e55d1b00
Simon Oulevay <simon.oulevay@gmail.com>
2020-02-08 20:47:44 +0100

    Set content type to application/hal+json on api root

--- server/apps/boardr_api/lib/boardr_api/api_root/api_root_controller.ex
+++ server/apps/boardr_api/lib/boardr_api/api_root/api_root_controller.ex
@@ -2,6 +2,8 @@ defmodule BoardrApi.ApiRootController do
   use BoardrApi, :controller
 
   def index(%Conn{} = conn, _params) do
-    render(conn, :index)
+    conn
+    |> put_resp_content_type("application/hal+json")
+    |> render(:index)
   end
 end
