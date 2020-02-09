8011105d6842bc905cdc4a45b0640f38f86cc5f3
Simon Oulevay <simon.oulevay@gmail.com>
2020-02-09 21:45:19 +0100

    Fix local game servers count

--- server/apps/boardr/lib/boardr/stats_server.ex
+++ server/apps/boardr/lib/boardr/stats_server.ex
@@ -30,7 +30,7 @@ defmodule Boardr.StatsServer do
   end
 
   defp count_local_game_servers() do
-    Swarm.registered() |> Enum.map(fn reg -> Map.fetch!(reg, :pid) end) |> Enum.filter(fn pid ->
+    Swarm.registered() |> Enum.map(fn {_, pid} -> pid end) |> Enum.filter(fn pid ->
       try do
         Process.alive?(pid)
       rescue
